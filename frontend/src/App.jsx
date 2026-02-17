5b88b210eb7a38908b0ba31baaf0de6428d5a417
jjjMarkiso <166040506+jjjMarkiso@users.noreply.github.com>
2026-02-16 18:02:25 -0700

    base files for website

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./pages/main";
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+        </Routes>
+      </Router>
+  )
+}
+
+export default App
